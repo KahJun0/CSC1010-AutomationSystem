9b677766ecb74a3080b1550fe6380c1928a6fccc
KJ <tongkj2000@gmail.com>
2022-03-20 23:15:25 +0800

    Moved flask into its own thread as it running on the main thread is causing issues (MQTT and GPIO malfunctions)

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 import sys
-
-import servo_control
-import rfid_controls
-import MQTT_Subscriber
 import threading
 
 # below are imports for the camera
 from flask import Flask, render_template, Response, request
+
+import MQTT_Subscriber
+import rfid_controls
+import servo_control
 from camera import VideoCamera
-import time
-import os
 
 # Flask stuff
 app = Flask(__name__)
+
 
 # Route to main page, index.html
 @app.route('/', methods=['GET', 'POST'])
 def move():
     result = ""
     if request.method == 'POST':
-        
         return render_template('index.html', res_str=result)
-                        
+
     return render_template('index.html')
 
 
@@ -30,6 +28,7 @@ def gen(camera):
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 
 # Video feed route
 @app.route('/video_feed')
@@ -45,7 +44,11 @@ if __name__ == '__main__':
     servo_control = servo_control.GroveServo(int(sys.argv[1]))
     MQTT_Thread = threading.Thread(target=MQTTSub.run, args=(servo_control,), daemon=True)
     RFID_Thread = threading.Thread(target=RFIDRead.run, args=(), daemon=True)
+    Flask_thread = threading.Thread(target=lambda: app.run(host='0.0.0.0', debug=True, use_reloader=False), daemon=True)
     MQTT_Thread.start()
     RFID_Thread.start()
-    app.run(host='0.0.0.0', debug=True, threaded=True)
+    Flask_thread.start()
+    MQTT_Thread.join()
+    RFID_Thread.join()
+    Flask_thread.join()
     print("Exiting")
